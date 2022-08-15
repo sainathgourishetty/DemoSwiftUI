98bf7680134f6d1a48be9884f76a1697912347a1
sainath <sainath@incrivelsoft.com>
2022-08-15 15:50:29 +0530

    Initial Commit

---
+++ DemoSwiftUI/DemoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwiftUIApp.swift
+//  DemoSwiftUI
+//
+//  Created by Karthik on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
